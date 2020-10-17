1fe03b120dcc7ccb9a472670b7a9cddf9caad307
JoshuaEckersley <josheckooo@hotmail.com>
2020-10-18 00:04:33 +1000

    Contacts resource controller and routes set up, changed login redirect to contacts index, contacts index view created and contacts data appear on page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('contacts', ContactController::class);
