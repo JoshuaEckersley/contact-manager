0458239394a86d4ec671ab58a0b4a8c35c65fdf4
JoshuaEckersley <josheckooo@hotmail.com>
2020-10-17 20:34:52 +1000

    created user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        // Hard code a few to make login easier during testing.
+        $this->hardCodeUser('user1');
+        $this->hardCodeUser('user2');
+        $this->hardCodeUser('user3');
+        $this->hardCodeUser('user4');
+    }
+
+    /**
+     * Create a user record with given name as the
+     * name and email.
+     */
+    private function hardCodeUser(string $name)
+    {
+        User::create([
+            'name' => $name,
+            'email' => $name . '@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
